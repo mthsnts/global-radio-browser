60791f8c06643ee9e0435a12b885f817af0b96a9
Matheus <matheussantostb1@gmail.com>
2025-03-14 22:45:49 -0300

    [feat] - Initial Commit

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface RadioStation {
+    changeuuid: string;
+    stationuuid: string;
+    name: string;
+    url: string;
+    url_resolved: string;
+    homepage: string;
+    favicon: string;
+    tags: string;
+    country: string;
+    countrycode: string;
+    language: string;
+    votes: number;
+    lastchangetime: string;
+    codec: string;
+    bitrate: number;
+    clickcount: number;
+    clicktrend: number;
+    ssl_error: number;
+    geo_lat: number;
+    geo_long: number;
+  }
